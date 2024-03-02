1804a40ed7e2f50831532a1a5582d4a0e8872f0b
NL1693 <amanmalik059@gmail.com>
2024-03-02 12:11:09 +0400

    Exercise_1 Completed

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_1.py
@@ -0,0 +1,13 @@
+# Assigning & Printing a Message, Then Reassigning & Reprinting the Message
+
+# Assigning 1st Message
+Message = "Hello There!"
+
+# Printing 1st Message
+print(Message)
+
+# Assigning 2nd Message
+Message = "Nice to meet you!"
+
+# Printing 2nd Message
+print(Message)
