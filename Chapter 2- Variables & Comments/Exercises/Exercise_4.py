286f5be570dc88c0e5e091fc7d7bcdc701639c88
NL1693 <amanmalik059@gmail.com>
2024-03-02 13:30:14 +0400

    Exercise_4 Completed

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_4.py
@@ -0,0 +1,10 @@
+# Defining Favorite Number and Printing it
+
+# Defining Favorite Number
+Fav_Num = 1693
+
+# Defining the Message
+Message = "My Favorite Number is " + str(Fav_Num) + "."
+
+# Printing the Message
+print(Message)
