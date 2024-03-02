ea125ce1ba8410e45c46109af22da54724b5cf48
NL1693 <amanmalik059@gmail.com>
2024-03-02 11:26:06 +0400

    Exercise_5 Completed

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_5.py
@@ -0,0 +1,13 @@
+# Calculating the Area of a User-Defined Circle
+
+# Taking Input from User    # Using Float in case of Radius Values with Decimals
+Radius = float(input("Enter the Circle's Radius: "))
+
+# Value of π up to 10 decimal places
+Pi = 3.1415926535
+
+# Calculating Area using Formula: Area = π*R^2 (R^2 = R*R)
+Area = Pi * Radius * Radius
+
+# Printing the Area of Circle
+print("Area of Circle with Radius", Radius, "is: ", Area)
