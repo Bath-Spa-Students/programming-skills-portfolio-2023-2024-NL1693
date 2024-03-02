91efb78fc3de8c7f419fd89d1be25d829c6f8af9
NL1693 <amanmalik059@gmail.com>
2024-03-02 10:44:05 +0400

    Exercise_4 Completed

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise_4.py
@@ -0,0 +1,9 @@
+# Defining Multiple Strings and Printing their Concatenation
+
+# Defining the Strings
+Var1 = "This is String 1."
+Var2 = "This is String 2."
+Var3 = "This is String 3."
+
+# Combining and Printing the Strings
+print(Var1, Var2, Var3)
