5d5d98b3e842d16774ada6c268b4d6154a4ab95e
NL1693 <amanmalik059@gmail.com>
2024-03-02 12:38:37 +0400

    Exercise_2 Completed

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise_2.py
@@ -0,0 +1,10 @@
+# Printing a Quote and it's Author
+
+# Defining the Quote
+Quote = "All that we are is the result of what we have thought."
+
+# Defining its Author
+Author = "Buddha"
+
+# Printing the Author and their Quote   # Using + to Ensure No Spacing between the Quote and "" Marks
+print(Author, "once said,", '"' + Quote + '"')
