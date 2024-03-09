3b3c9fa9bf4019bba37fc95a3dd44f5a1eae3416
NL1693 <amanmalik059@gmail.com>
2024-03-09 10:26:30 +0400

    Exercise_1 Completed

---
+++ Chapter 3- Data Structures/Exercises/Exercise_1.py
@@ -0,0 +1,13 @@
+# Individually Printing Friend Names from List
+
+# Storing Names in List
+Names = ["Youssouf", "Suhayb", "Shawn", "John", "Vinz", "Mutahhar", "Aboody"]
+
+# Printing Names Individually
+print(Names[0])
+print(Names[1])
+print(Names[2])
+print(Names[3])
+print(Names[4])
+print(Names[5])
+print(Names[6])
