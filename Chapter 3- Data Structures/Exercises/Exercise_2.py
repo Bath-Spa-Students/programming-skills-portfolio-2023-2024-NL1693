7a41537df739d7c4c0ab2a1d9a2dd3d51e69b266
NL1693 <amanmalik059@gmail.com>
2024-03-09 10:30:13 +0400

    Exercise_2 Completed

---
+++ Chapter 3- Data Structures/Exercises/Exercise_2.py
@@ -0,0 +1,8 @@
+# Personalized Messages to Friends in List
+
+# Storing Names in List
+Names = ["Youssouf", "Suhayb", "Shawn", "John", "Vinz", "Mutahhar", "Aboody"]
+
+# Printing Personalized Message using For Loop
+for Name in Names:
+    print("Hello", Name + ",", "How are you?")
