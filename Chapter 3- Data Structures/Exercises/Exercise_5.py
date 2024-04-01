ec9f2f7ededd2ff027a938a553cd48d1487445b0
NL1693 <amanmalik059@gmail.com>
2024-04-01 10:12:24 +0400

    Exercise_5 Complete

---
+++ Chapter 3- Data Structures/Exercises/Exercise_5.py
@@ -0,0 +1,33 @@
+# List of Guests Invited to Dinner
+Guest = ['Sir Isaac Newton', 'Charles Darwin', 'Albert Einstein']
+
+# Inviting 1st Guest
+Name = Guest[0]
+print (f"\n{Name}, I'd be honored to have you for dinner.")
+
+# Inviting 2nd Guest
+Name = Guest[1]
+print (f"{Name}, Would you kindly attend our dinner?")
+
+# Inviting 3rd Guest
+Name = Guest[2]
+print (f"{Name}, Could you grace our table for dinner?\n")
+
+# Printing the Guest who can't make it
+Name = Guest[0]
+print (f"{Name} can't make it to Dinner.\n")
+
+# Replacing Guest with New Guest
+Guest[0] = 'Alan Turing'
+
+# Inviting New 1st Guest
+Name = Guest[0]
+print (f"{Name}, I'd be honored to have you for dinner.")
+
+# Inviting 2nd Guest Again
+Name = Guest[1]
+print (f"{Name}, Would you kindly attend our dinner?")
+
+# Inviting 3rd Guest Again
+Name = Guest[2]
+print (f"{Name}, Could you grace our table for dinner?\n")
