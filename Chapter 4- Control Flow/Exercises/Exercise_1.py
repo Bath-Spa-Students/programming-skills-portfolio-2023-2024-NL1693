609a9c577b4c5ff1c1f9c9a0cde6495ee31ba71c
NL1693 <amanmalik059@gmail.com>
2024-04-01 10:52:12 +0400

    Exercise_1 Completed

---
+++ Chapter 4- Control Flow/Exercises/Exercise_1.py
@@ -0,0 +1,13 @@
+# Variable with Color Assigned
+alien_color = 'green'
+
+# If Alien is Green, Give 5 Points      # Version = Passes
+if alien_color == 'green':
+    print ("+5 Points Earned!")
+
+# Variable with Color Assigned
+alien_color = 'red'
+
+# If Alien is Green, Give 5 Points      # Version = Fails
+if alien_color == 'green':
+    print ("+5 Points Earned!")
