c1bca29525dd75e6faf0432e3f3046b99d30e5c0
NL1693 <amanmalik059@gmail.com>
2024-04-01 11:21:37 +0400

    Exercise_4 Completed

---
+++ Chapter 4- Control Flow/Exercises/Exercise_4.py
@@ -0,0 +1,26 @@
+# Age of Person
+Age = 20
+
+# Less Than 2
+if Age < 2:
+    print("This is a Baby.")
+
+# Less Than 4 & More Than 2
+elif Age < 4:
+    print("This is a Toddler.")
+
+# Less Than 13 & More Than 4
+elif Age < 13:
+    print("This is a Kid.")
+
+# Less Than 20 & More Than 13
+elif Age < 20:
+    print("This is a Teenager.")
+
+# Less Than 65 & More Than 20
+elif Age < 65:
+    print("This is an Adult.")
+
+# 65 or More
+else:
+    print("This is an Elder.")
