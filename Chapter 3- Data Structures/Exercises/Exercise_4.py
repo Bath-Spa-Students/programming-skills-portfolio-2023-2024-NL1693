c095c96f1289587ef0dd5e7b36dcd00b551cf959
NL1693 <amanmalik059@gmail.com>
2024-04-01 10:03:56 +0400

    Exercise_4 Completed

---
+++ Chapter 3- Data Structures/Exercises/Exercise_4.py
@@ -0,0 +1,14 @@
+# List of Guests Invited to Dinner
+Guest = ['Sir Isaac Newton', 'Charles Darwin', 'Albert Einstein']
+
+# Inviting 1st Guest
+Name = Guest[0]
+print (f"{Name}, I'd be honored to have you for dinner.")
+
+# Inviting 2nd Guest
+Name = Guest[1]
+print (f"{Name}, Would you kindly attend our dinner?")
+
+# Inviting 3rd Guest
+Name = Guest[2]
+print (f"{Name}, Could you grace our table for dinner?")
