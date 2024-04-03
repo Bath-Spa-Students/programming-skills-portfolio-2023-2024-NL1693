e86b05cc0b9b45c73d4cf0004f35015fcc49e77a
NL1693 <amanmalik059@gmail.com>
2024-04-03 06:53:44 +0400

    Exercise_2 Completed

---
+++ Chapter 5- Dictionaries/Exercises/Exercise_2.py
@@ -0,0 +1,12 @@
+# Glossary with Programming Terms & Meanings
+Glossary = {
+    "Variable": "A Container that stores a Value.",
+    "Function": "A Block of Reuseable Code that Performs a Set Task.",
+    "Loop": "A Block of Code that Repeats Execution until a Specific Condition is True.",
+    "List": "A Collection of Ordered Items. Can be Changed or Sorted",
+    "Conditional Statement": "A Statement that Executes Different Actions based on a True or False Condition."
+}
+
+# Print Term & Meaning in Neat Format
+for Term, Meaning in Glossary.items():
+    print(Term + ": " + Meaning + "\n")
