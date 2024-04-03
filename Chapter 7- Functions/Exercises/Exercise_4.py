3016922daf876b69a52a552ff5801aa865d0ae0f
NL1693 <amanmalik059@gmail.com>
2024-04-03 10:14:18 +0400

    Exercise_4 Completed

---
+++ Chapter 7- Functions/Exercises/Exercise_4.py
@@ -0,0 +1,12 @@
+# make_shirt Function(Parameter, Parameter)
+def make_shirt(size="Large", message="I Love Python"):
+    print(f'Size of Shirt is {size} & Printed Message is "{message}".')
+
+# Call Function with Default Arguments
+make_shirt()
+
+# Call Function with Only Size Argument
+make_shirt("Medium")
+
+# Call Function with Both Arguments
+make_shirt("Extra-Large", "Eat Sleep Code Repeat")
